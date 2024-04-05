56f2e7ad6477813b1b8ebfcfbe96dbfe5d131a73
Alfredo Rodriguez Bravo <arodribr@gmail.com>
2024-04-05 02:59:26 +0200

    ChannelResults component and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import styles from './App.module.css';
-import { HeaderSeeker } from './components/HeaderSeeker/HeaderSeeker';
-import { SearchResults } from './components/SearchResults/SearchResults';
-import { usePodcasts } from './hooks/usePodcasts';
-import { useSearch } from './hooks/useSearch';
-import { SearchPodcasts } from './services/SearchPodcasts';
+import { Route, Routes, useNavigate } from "react-router-dom";
+import styles from "./App.module.css";
+import { ChannelResults } from "./components/ChannelResults/ChannelResults";
+import { HeaderSeeker } from "./components/HeaderSeeker/HeaderSeeker";
+import { SearchResults } from "./components/SearchResults/SearchResults";
+import { usePodcasts } from "./hooks/usePodcasts";
+import { useSearch } from "./hooks/useSearch";
+import { SearchPodcasts } from "./services/SearchPodcasts";
 function App () {
     const { hasResults, podcasts, handleSetPodcasts } = usePodcasts();
     const { search, error, updateSearch } = useSearch();
     const navigate = useNavigate();
     const handleSearchSubmit = async (
-        event: React.FormEvent<HTMLFormElement>,
+        event: React.FormEvent<HTMLFormElement>
     ): Promise<void> => {
         event.preventDefault();
         const podcast = await SearchPodcasts( { search } );
         handleSetPodcasts( podcast );
-        navigate( '/' );
+        navigate( "/" );
     };
     const handleInputChange = ( event: React.FormEvent<HTMLInputElement> ) => {
         updateSearch( event.currentTarget.value );
     };
     return (
         <div className={styles.page}>
-            <HeaderSeeker search={search} error={error} searchSubmit={handleSearchSubmit} inputChange={handleInputChange} />
+            <HeaderSeeker
+                search={search}
+                error={error}
+                searchSubmit={handleSearchSubmit}
+                inputChange={handleInputChange}
+            />
             <Routes>
-                <Route path='/' element={<SearchResults hasResults={hasResults} podcasts={podcasts} />} />
+                <Route
+                    path='/'
+                    element={
+                        <SearchResults
+                            hasResults={hasResults}
+                            podcasts={podcasts}
+                        />
+                    }
+                />
+                <Route
+                    path='/channel'
+                    element={<ChannelResults />}
+                />
             </Routes>
         </div>
     );
---
+++ src/components/ChannelResults/ChannelResults.tsx
@@ -0,0 +1,8 @@
+import styles from "./ChannelResults.module.css";
+export const ChannelResults = ( props ) => {
+    return (
+        <>
+            <div className={styles.channelResults}>Hello Channel</div>
+        </>
+    );
+};
